26f56f05b90fb7ea47b211abbdd72d6b144f9200
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2023-11-09 09:17:11 +0000

    DataInspector: Implement the stack tab
    This will allow users to inspect the values of local variables, and
    possibly in the future parameters.

--- pcsx2/DebugTools/DebugInterface.h
+++ pcsx2/DebugTools/DebugInterface.h
@@ -65,6 +65,8 @@ public:
 	virtual void write8(u32 address, u8 value) = 0;
 	virtual void write16(u32 address, u16 value) = 0;
 	virtual void write32(u32 address, u32 value) = 0;
+	virtual void write64(u32 address, u64 value) = 0;
+	virtual void write128(u32 address, u128 value) = 0;
 
 	// register stuff
 	virtual int getRegisterCategoryCount() = 0;
@@ -119,6 +121,8 @@ public:
 	void write8(u32 address, u8 value) override;
 	void write16(u32 address, u16 value) override;
 	void write32(u32 address, u32 value) override;
+	void write64(u32 address, u64 value) override;
+	void write128(u32 address, u128 value) override;
 
 	// register stuff
 	int getRegisterCategoryCount() override;
@@ -160,6 +164,8 @@ public:
 	void write8(u32 address, u8 value) override;
 	void write16(u32 address, u16 value) override;
 	void write32(u32 address, u32 value) override;
+	void write64(u32 address, u64 value) override;
+	void write128(u32 address, u128 value) override;
 
 	// register stuff
 	int getRegisterCategoryCount() override;
